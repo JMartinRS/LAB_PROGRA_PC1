60d789569e6520ade82dc426efef1ca797fd6f41
J Martín RS <juan_rodriguez9@usmp.pe>
2020-10-19 12:32:10 -0500

    main

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LAB_PROGRA_PC1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
